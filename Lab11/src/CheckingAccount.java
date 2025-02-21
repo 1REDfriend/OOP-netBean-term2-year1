85c90e8c267f4c14438ed4e387f17af603427b5e
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2025-02-21 09:42:33 +0700

    klshfiuh

---
+++ Lab11/src/CheckingAccount.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author tongt
+ */
+public class CheckingAccount extends Account {
+
+    private double credit;
+
+    public CheckingAccount() {
+        super(0, "");
+        this.credit = 0;
+    }
+
+    public CheckingAccount(double balance, String name, double credit) {
+        super(balance, name);
+        this.credit = credit;
+    }
+
+    public void setCredit(double credit) {
+        if (credit > 0) {
+            this.credit = credit;
+        } else {
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    @Override
+    public void withdraw(double a) throws WithdrawException {
+        if (a <= 0) {
+        throw new WithdrawException("Input number must be a positive integer.");
+        }
+        if (a > 0) {
+            double remaining = this.getBalance() - a;
+            if (remaining >= 0) {
+                this.setBalance(remaining);
+                System.out.println(a + " baht is withdrawn from " + this.getName() + " and your credit balance is " + credit + ".");
+            } else if (remaining + credit >= 0) {
+                credit += remaining;
+                this.setBalance(0);
+                System.out.println(a + " baht is withdrawn from " + this.getName() + " and your credit balance is " + credit + ".");
+            } else {
+                throw new WithdrawException("Account "+super.getName()+" has not enough money!");
+            }
+        }
+    }
+
+    public void withdraw(String a)throws WithdrawException {
+        double number = Double.parseDouble(a);
+        this.withdraw(number);
+    }
+
+    @Override
+    public String toString() {
+        return "The "+this.getName()+" account has "+this.getBalance()+" baht and "+credit+" credits.";
+    }
+}
